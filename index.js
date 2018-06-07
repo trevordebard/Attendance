3ae029811e2282f7536f62907b3ab57e6093fdb6
Trevor DeBardeleben <trevordebard@gmail.com>
2018-06-06 22:15:40 -0500

    Improve styling; modify route handling

--- index.js
+++ index.js
@@ -12,63 +12,62 @@ const db  = require('./db');
 db.connect(); //connect to the database
 
 app.use(session);
-app.use(express.static(__dirname + '/public'));
 
-app.get('/room', (req,res) => {
-  req.session.uid = req.sessionID;
-  console.log('eventually ');
-  console.log(req.session);
-  if(!req.query.id) {
-    return res.send('Please enter the room you would like to join');
-  }
-  db.doesRoomExist(req.query.id, (err, response)=> {
-    if(err || !response) {
-      console.log(err); 
-      res.send('Whoops! It appears that room does not exist.')
-    }
-    else {
-      res.sendFile(__dirname + '/public/room.html');
-    }
-  });
-});
-
-app.get('/:room', (req, res, next) => { 
-  res.redirect('/room?id='+req.params.room);
-});
- 
 const port = process.env.PORT || 4000;
 const server = app.listen(port, () => {
   console.log('listening for requests on port ' + port);
 });
+app.use(express.static(__dirname + '/public/'));
+
+app.get('/', (req,res)=> {
+  console.log('yo');
+})
+app.get('/join/:room', (req, res) => { 
+  req.session.uid = req.sessionID;
+  console.log('eventually ');
+  console.log(req.session.hasCreated);
+
+  db.doesRoomExist(req.params.room, (err, response)=> {
+    if(err || !response) {
+      res.send('error');
+    }
+    else {
+      if(req.session.hasCreated) {
+        if((req.session.roomsCreated).includes(`${req.params.room}`)) {
+          console.log('you created this room');
+        }
+        else {
+          console.log('you did not create this room, but you did create others')
+        }
+      }
+      else {
+        console.log('you have not created any rooms yet. Just a user');
+      }
+      console.log(req.session.roomsCreated);
+      res.sendFile( __dirname + "/public/" + "room.html" );
+    }
+  });
+});
 
 const io = require('socket.io')(server);
-io.use(sharedsession(session));
+io.use(sharedsession(session, ));
 io.on('connection', (socket) => {
   //Get the id the user is requesting
   console.log('connected');
-  //console.log(socket.handshake.session);
+  if(socket.handshake.session.uid == null) {
+    socket.handshake.session.uid = socket.id;
+    socket.handshake.session.save();
+  }
+  console.log(socket.handshake.session);
   const session = socket.handshake.session.uid;
-
-  let url = socket.handshake.headers.referer.split('/');
-  let route = url.pop();
-  let id = -1;
-  if(route.substring(0, 8) == "room?id=") {
-    id = route.substring(8)
-  }
-  else {
-    console.log("Could not retrieve id");
-  }
-  let room = -1;
-  if(id != -1) {
-    room = id;
-    socket.join(room);
-    getUsers(room);
-  }
+  console.log('session socket');
+ 
 
   socket.on('new-user', (data) => {
-    db.doesSessionExist(room, session, (err, response) => {
+    db.doesSessionExist(data.room, session, (err, response) => {
       if(err) {
         console.log('Error determining whether session exists');
+        console.log(err);
       }
       else {
         if(response) {
@@ -78,13 +77,13 @@ io.on('connection', (socket) => {
           /**
            * Add the user to the database
            */
-          db.addUser(room, data.name, socket.handshake.session.uid, (err, response) => {
+          db.addUser(data.room, data.name, socket.handshake.session.uid, (err, response) => {
             if(err) {
               console.log('insert failed');
               console.log(err);
             }
             else { 
-              getUsers(room);
+              getUsers(data.room);
             }
           });
         }
@@ -108,7 +107,34 @@ io.on('connection', (socket) => {
       else {
         console.log(res);
       }
+      socket.handshake.session.hasCreated = true;
+      if(!socket.handshake.session.roomsCreated) {
+        socket.handshake.session.roomsCreated = [`${data.roomId}`];
+      }
+      else {
+        (socket.handshake.session.roomsCreated).push(`${data.roomId}`);
+      }
+      socket.join(data.roomId);
+      socket.handshake.session.save();
+      //socket.emit('redirect', data.roomId);
     });
+  });
+
+  socket.on('does-room-exist', (roomId) => {
+    console.log(roomId);
+    db.doesRoomExist(roomId, (err, response)=> {
+      if(err || !response) {
+        console.log(err); 
+        console.log('Whoops It appears that room does not exist.')
+      }
+      else {
+        socket.emit('room-exists');
+      }
+    });
+  });
+  socket.on('join-room', (roomId) => {
+    console.log('attempted to join');
+    socket.join(roomId);
   });
 });
 
