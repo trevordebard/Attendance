50789749d9b731850732f9ef9ea00c0c442f828c
Trevor DeBardeleben <trevordebard@gmail.com>
2018-07-08 14:46:07 -0500

    Revert "fix for heroku"
    This reverts commit 76f86c0f6336b02885a1ede656d15386457ad9e3.

--- src/server/server.js
+++ src/server/server.js
@@ -4,10 +4,10 @@ const express = require('express');
 const dotenv = require('dotenv');
   dotenv.config();
 const db = require('./db');
-db.connect();
-const PORT = process.env.PORT || 4006;
+  db.connect();
 const routes = require('./routes')(app);
 const dev = app.get('env') !== 'production';
+const PORT = process.env.SERVER_PORT || 4006;
 
 const server = app.listen(PORT, () => {
   console.log(`listening for requests on port ${PORT}`);
