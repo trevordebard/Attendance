7d5f2d7f58a3dfb5ed898b8615c1550a95de8198
Trevor DeBardeleben <trevordebard@gmail.com>
2018-07-08 14:05:38 -0500

    change url

--- src/api.js
+++ src/api.js
@@ -1,28 +1,29 @@
 const fetch = require('node-fetch');
-
+const url = process.env.REACT_APP_ORIGIN_URL + ':' + process.env.REACT_APP_PORT + '/api';
+console.log('api url is: ' + url);
 function createRoom(roomId) {
-  return fetch(`${process.env.REACT_APP_API_URL}/createRoom/${roomId}`)
+  return fetch(`${url}/createRoom/${roomId}`)
     .then(res => res.json())
     .then(data => (data))
     .catch(error => ('There was a problem processing your request'));
 }
 
 function doesRoomExist(roomId) {
-  return fetch(`${process.env.REACT_APP_API_URL}/doesRoomExist/${roomId}`)
+  return fetch(`${url}/doesRoomExist/${roomId}`)
     .then(res => res.json())
     .then(data => (data))
     .catch(error => ('There was a problem processing your request'));
 }
 
 function addUser(roomId, name) {
-  return fetch(`${process.env.REACT_APP_API_URL}/addUser/${roomId}/${name}`)
+  return fetch(`${url}/addUser/${roomId}/${name}`)
     .then(res => res.json())
     .then(data => data)
     .catch(error => error);
 }
 
 function getUsers(roomId) {
-  return fetch(`${process.env.REACT_APP_API_URL}/getUsers/${roomId}`)
+  return fetch(`${url}/getUsers/${roomId}`)
     .then(res => res.json())
     .then(data => data)
     .catch(error => error);
