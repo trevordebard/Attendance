2d981296508ffd365014069d16abbe4acbd1ee9b
Trevor DeBardeleben <trevordebard@gmail.com>
2018-07-13 18:29:23 -0500

    Prevent users from generating too many rooms; Add SMI logo

--- src/components/BoxHome.js
+++ src/components/BoxHome.js
@@ -15,6 +15,7 @@ class BoxHome extends Component {
 			roomCodeInvalid: false,
 			roomCodeInput: '',
 			roomCode: roomCode,
+			tooManyAttempts: false,
 		}
 	}
 
@@ -23,11 +24,43 @@ class BoxHome extends Component {
 			roomCodeInput: e.target.value
 		})
 	}
+	array = ['timestamp1', 'timestamp2', 'timestamp3', ]
 	handleCreateRoom = () => {
+		let roomsCreatedInLastHour = window.localStorage.getItem('creationTimestamps');
+		let creationTimestamps = JSON.parse(roomsCreatedInLastHour);
+		let count = 0;
+		let currentTime = new Date().getTime();
+		
 
+		if(roomsCreatedInLastHour) {
+			let del = [];
+			// Iterate through array of time
+			for(let i=0; i < creationTimestamps.length; i++){
+				if(creationTimestamps[i] > currentTime - 3600000) { //if the timestamp occurred within the last hour
+					count++;
+				}
+				else {
+					del.push(i);
+				}
+			}
+			// Delete any timestamps over an hour old
+			for(let i in del) {
+				creationTimestamps.splice(i, 1);
+			}
+		}
+		else {
+			creationTimestamps = [currentTime];
+		}
+		if(count > 5) {
+			this.setState({
+				tooManyAttempts: true,
+			})
+		}
+		else {
+			creationTimestamps.push(currentTime);
+			window.localStorage.setItem('creationTimestamps', JSON.stringify(creationTimestamps))
 			createRoom(`${Math.random().toString(36).substr(2, 5)}`)
 			.then((data) => {
-				console.log(data);
 				if (data.success) {
 					//this.props.toggleBoxHome(false, data.room, 'BoxRoom');
 					//this.props.match.history.push(`/room/${data.room}`);
@@ -43,6 +76,8 @@ class BoxHome extends Component {
 					})
 				}
 			})
+		}
+		
 	}
 
 	enterRoomCode = (e) => {
@@ -83,16 +118,18 @@ class BoxHome extends Component {
 	render() {
 		return (
 			<div className="box">
+			<img src={require('../images/SMI_logo.png')} style={{width: '260px', height: '65px'}} alt='Sign Me In'/>
 				<div className='header'>
-					<h2>{this.state.title}</h2>
 					<hr/>
 				</div>
 				<div className="content">
 					<button id="create-room" onClick={this.handleCreateRoom}>
 						Generate room
 					</button>
+					{this.state.tooManyAttempts &&
+						<p className="box-validation">You're doing that too much. Try again in an hour.</p>
+					}
 					<Divider id='or' horizontal>or</Divider>
-
 					<span>
 						<input onKeyDown={this.enterRoomCode} onChange={this.handleRoomCodeInput} type="text" id="room-code-input" placeholder="Enter room code" />
 						<i id="enter-room-btn" onClick={this.joinRoom} className="fa fa-angle-right" />
