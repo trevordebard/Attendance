b566664a088a617914d593e540167b4c6c622c69
Trevor DeBardeleben <trevordebard@gmail.com>
2018-07-14 13:06:17 -0500

    Update consts.js

--- src/consts.js
+++ src/consts.js
@@ -1,4 +1,4 @@
-const dev = (process.NODE_ENV !== 'production');
+const dev = (process.env.NODE_ENV !== 'production');
 let api_url;
 if(dev) {
     api_url = 'https://smi-dev-api.herokuapp.com/api'
